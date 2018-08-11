2a3b67cb9cb57e4f8590083076790140205dca73
José Vidal <javidgon@gmail.com>
2018-08-11 16:04:07 +0200

    Add additional JOB_STATUSES

--- sharedcloud.py
+++ sharedcloud.py
@@ -4,13 +4,26 @@ import requests
 import os
 import time
 import shutil
+import timeago
+from tabulate import tabulate
 from subprocess import check_output, CalledProcessError
 
-
+DATETIME_FORMAT = '%d-%m-%Y %H:%M:%S'
 BASE_URL = os.environ.get('BASE_URL', 'http://142.93.102.53:8000')
 CONFIG_FOLDER = '{}/.sharedcloud'.format(os.path.expanduser('~'))
 CONFIG_FILE = '{}/config'.format(CONFIG_FOLDER)
 
+
+JOB_STATUSES = {
+    'WAITING': 1,
+    'IN_PROGRESS': 2,
+    'SUCCEEDED': 3,
+    'FAILED': 4,
+    'TIMEOUT': 5
+}
+
+
+now = datetime.datetime.now()
 
 class Config(object):
 
@@ -85,6 +98,30 @@ def create(config, name, language, code):
         print(r.content)
 
 
+@task.command(help='List Tasks')
+@pass_config
+def list(config):
+    # sharedcloud task list"
+    if not config.token:
+        print('Please log in first')
+        return
+
+    r = requests.get('{}/tasks/'.format(BASE_URL),
+                     headers={'Authorization':'Token {}'.format(config.token)})
+
+    if r.status_code == 200:
+        tasks = r.json()
+
+        click.echo(tabulate([[
+            task.get('uuid'),
+            task.get('name'),
+            task.get('programming_language'),
+            timeago.format(datetime.datetime.strptime(task.get('created_at'), DATETIME_FORMAT), now),
+        ] for task in tasks], headers=['UUID', 'NAME', 'LANGUAGE', 'CREATED_AT']))
+    else:
+        print(r.content)
+
+
 @task.command(help='Deletes a Task')
 @click.option('--uuid', required=True)
 @pass_config
@@ -128,6 +165,65 @@ def create(config, task_uuid, num_jobs):
     else:
         print(r.content)
 
+
+@run.command(help='List Runs')
+@pass_config
+def list(config):
+    # sharedcloud task list"
+    if not config.token:
+        print('Please log in first')
+        return
+
+    r = requests.get('{}/runs/'.format(BASE_URL),
+                     headers={'Authorization':'Token {}'.format(config.token)})
+
+    if r.status_code == 200:
+        runs = r.json()
+
+        click.echo(tabulate([[
+            run.get('uuid'),
+            run.get('num_jobs'),
+            timeago.format(datetime.datetime.strptime(run.get('created_at'), DATETIME_FORMAT), now),
+            run.get('task')
+        ] for run in runs], headers=['UUID', 'NUM_JOBS', 'CREATED_AT', 'TASK_UUID']))
+    else:
+        print(r.content)
+
+
+@cli1.group(help='List Jobs')
+@pass_config
+def job(config):
+    pass
+
+@job.command(help='List Jobs')
+@pass_config
+def list(config):
+    def _map_status_to_description(status_id):
+        for status_name, id in JOB_STATUSES.items():
+            if id == status_id:
+                return status_name
+
+    # sharedcloud task list"
+    if not config.token:
+        print('Please log in first')
+        return
+
+    r = requests.get('{}/jobs/'.format(BASE_URL),
+                     headers={'Authorization':'Token {}'.format(config.token)})
+
+    if r.status_code == 200:
+        jobs = r.json()
+
+        click.echo(tabulate([[
+            job.get('uuid'),
+            job.get('incremental_id'),
+            _map_status_to_description(job.get('status')),
+            job.get('cmd_output'),
+            timeago.format(datetime.datetime.strptime(job.get('created_at'), DATETIME_FORMAT), now),
+            job.get('run')
+        ] for job in jobs], headers=['UUID', 'ID', 'STATUS', 'CMD_OUTPUT', 'CREATED_AT', 'RUN_UUID']))
+    else:
+        print(r.content)
 
 @cli1.group(help='Register/Start/List Instances')
 @pass_config
@@ -200,52 +296,52 @@ def start(config, uuid):
             text_file.write(data)
 
     def _generate_image(job_uuid, job_folder):
-        output = b''
+        build_output = b''
         image_tag = job_uuid
 
         docker_build = check_output(
             ['docker', 'build', '-t', '{}:latest'.format(image_tag),
              '-f', '{}/{}/Dockerfile'.format(CONFIG_FOLDER, job_uuid), job_folder])
         for line in docker_build.splitlines():
-            output += line + b'\n'
-        return image_tag, output
+            build_output += line + b'\n'
+        return image_tag, build_output
 
     def _run_container(image_tag):
-        result = b''
+        cmd_output = b''
         container_tag = image_tag
+        has_timeout = False
 
         try:
             docker_run = check_output(
                 ['docker', 'run', '--memory=512m', '--cpus=1', '--name', container_tag, '{}:latest'.format(image_tag)])
             for line in docker_run.splitlines():
-                result += line + b'\n'
+                cmd_output += line + b'\n'
         except CalledProcessError as grepexc:
             # When it exists due to a timeout we don't exit the cli as it's kind of an expected error
-            if grepexc.returncode != 124:
+            if grepexc.returncode == 124:
+                has_timeout = True
+            else:
                 raise
-        return container_tag, result
 
-    def _destroy_container(container_tag, output):
+        return container_tag, cmd_output, has_timeout
+
+    def _destroy_container(container_tag, build_output):
         docker_destroy_container = check_output(
             ['docker', 'rm', container_tag, '--force'])
         for line in docker_destroy_container.splitlines():
-            output += line + b'\n'
+            build_output += line + b'\n'
 
-        return output
+        return build_output
 
-    def _destroy_image(image_tag, output):
+    def _destroy_image(image_tag, build_output):
         docker_destroy_image = check_output(
             ['docker', 'rmi', image_tag, '--force'])
         for line in docker_destroy_image.splitlines():
-            output += line + b'\n'
+            build_output += line + b'\n'
 
-        return output
+        return build_output
 
     instance_uuid = uuid
-
-    JOB_STATUS_WAITING = 1
-    JOB_STATUS_IN_PROGRESS = 2
-    JOB_STATUS_FINISHED = 3
 
     # sharedcloud start_instance --instance <uuid>
     if not config.token:
@@ -253,8 +349,8 @@ def start(config, uuid):
         return
 
     job_uuid = None
-    output = b''
-    result = b''
+    build_output = b''
+    cmd_output = b''
 
     try:
         # First, we let our remote know that we are starting the instance
@@ -280,7 +376,7 @@ def start(config, uuid):
                 # We update the job in the remote, so it doesn't get assigned to other instances
                 _make_patch_request(
                     job_uuid, {
-                        "status": JOB_STATUS_IN_PROGRESS,
+                        "status": JOB_STATUSES['IN_PROGRESS'],
                         "started_at": job_started_at
                     }, config.token)
 
@@ -299,21 +395,21 @@ def start(config, uuid):
 
                 # After the files have been created, we can generate the image that we are going to
                 # use to run our container
-                image_tag, output = _generate_image(job_uuid, job_folder)
+                image_tag, build_output = _generate_image(job_uuid, job_folder)
                 # After the image has been generated, we run our container and calculate our result
-                container_tag, result = _run_container(image_tag)
+                container_tag, cmd_output, has_timeout = _run_container(image_tag)
 
                 # After this has been done, we make sure to clean up the image, container and job folder
-                output = _destroy_container(container_tag, output)
-                output = _destroy_image(image_tag, output)
+                build_output = _destroy_container(container_tag, build_output)
+                build_output = _destroy_image(image_tag, build_output)
                 shutil.rmtree(job_folder)
 
                 # Finally, we let our remote know that job's stats
                 _make_patch_request(
                     job_uuid, {
-                        "log_output": output,
-                        "result": result,
-                        "status": JOB_STATUS_FINISHED,
+                        "build_output": build_output,
+                        "cmd_output": cmd_output,
+                        "status": JOB_STATUSES['SUCCEEDED'] if not has_timeout else JOB_STATUSES['TIMEOUT'],
                         "finished_at": datetime.datetime.now(),
                         "cost": round(
                             int((datetime.datetime.now()-job_started_at).total_seconds()) * (instance_price_per_hour/3600), 3)
@@ -327,12 +423,12 @@ def start(config, uuid):
 
     except Exception as e:
         # If the error was provoked by a job, we update our remote with the output
-        if job_uuid and output and result:
+        if job_uuid and build_output and cmd_output:
             _make_patch_request(
                 job_uuid, {
-                    "log_output": output,
-                    "result": result,
-                    "status": JOB_STATUS_FINISHED,
+                    "build_output": build_output,
+                    "cmd_output": cmd_output,
+                    "status": JOB_STATUSES['FAILED'],
                     "finished_at": datetime.datetime.now(),
                     "cost": 0.0 # Failed jobs are free
                     }, config.token)
