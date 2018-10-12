4406108521fb37e703b20a6d94c23c2e4c771472
José Vidal <javidgon@gmail.com>
2018-10-12 09:00:48 +0200

    Remove the 'examples' folder. Now it's part of a different repository

--- examples/simple_func_node8.js
+++
@@ -1,4 +0,0 @@
-function handler(event) {
-    console.log('NodeJS version: ' + process.version);
-    return 'Hello friend, you are passing ' + event.length + ' arguments';
-}
--- examples/simple_func_python36.py
+++
@@ -1,6 +0,0 @@
-import platform
-import time
-
-def handler(event):
-    print('Python version: {}'.format(platform.python_version()))
-    return 'Hello friend, you are passing {} arguments'.format(len(event))
